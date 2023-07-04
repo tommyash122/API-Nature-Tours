5d5b76560375c3f6fb611654629ea0bff463df91
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2023-07-04 18:37:14 +0300

    connect to DB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((con) => {
+    console.log('Connect success to database');
+  });
 
 const PORT = 3000 || process.env.PORT;
 
