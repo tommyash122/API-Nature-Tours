838e0c572e72878e90ccc7f24c1796675cd426a3
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2023-07-04 18:52:34 +0300

    create model

--- server.js
+++ server.js
@@ -7,19 +7,40 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-);
-
+  );
+  
+  const DB_local = process.env.DATABASE_LOCAL;
+  const PORT = 3000 || process.env.PORT;
+  
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
+    useUnifiedTopology: true,
   })
-  .then((con) => {
+  .then((_) => {
     console.log('Connect success to database');
   });
 
-const PORT = 3000 || process.env.PORT;
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 
 // Start the server
 app.listen(PORT, () => {
